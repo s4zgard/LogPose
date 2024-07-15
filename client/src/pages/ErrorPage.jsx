bc30ff86dad64525e50709c94978d5e55eb65f37
s4zgard <s4zgard@gmail.com>
2024-07-15 14:53:07 +0500

    Error page setup

--- client/src/pages/ErrorPage.jsx
+++ client/src/pages/ErrorPage.jsx
@@ -1,11 +1,29 @@
-import { Link } from "react-router-dom";
+import { Link, useRouteError } from "react-router-dom";
+import Wrapper from "../assets/wrappers/ErrorPage.js";
+import img404 from "../assets/images/not-found.svg";
 
 const ErrorPage = () => {
+  const error = useRouteError();
+
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img404} alt="404 not found" />
+          <h3>Lost in the Grand Line?</h3>
+          <p>Looks like you've pulled a Zoro and lost your way!.</p>
+          <Link to="/dashboard">Go back</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
   return (
-    <>
-      <h1>ErrorPage</h1>
-      <Link to="/">Home</Link>
-    </>
+    <Wrapper>
+      <div>
+        <h3>Something went wrong.</h3>
+      </div>
+    </Wrapper>
   );
 };
 export default ErrorPage;
