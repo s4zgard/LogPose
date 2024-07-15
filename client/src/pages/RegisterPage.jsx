6bd3d74a9b1c618023902a02aca75486aed3567e
s4zgard <s4zgard@gmail.com>
2024-07-15 15:03:08 +0500

    Register page setup

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -1,11 +1,40 @@
 import { Link } from "react-router-dom";
+import { Logo } from "../components";
+import Wrapper from "../assets/wrappers/RegisterAndLoginPage";
 
 const RegisterPage = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
   return (
-    <div>
-      <h1>RegisterPage</h1>
-      <Link to="/login">Login</Link>
-    </div>
+    <Wrapper>
+      <form onSubmit={handleSubmit} className="form">
+        <Logo />
+        <h4>Register</h4>
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            Name
+          </label>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            placeholder="Enter your name ..."
+            className="form-input "
+            required
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          Register
+        </button>
+        <p>
+          Already a member?{" "}
+          <Link to="/login" className="member-btn">
+            Login here
+          </Link>
+        </p>
+      </form>
+    </Wrapper>
   );
 };
 export default RegisterPage;
